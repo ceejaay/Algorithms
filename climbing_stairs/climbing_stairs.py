6acc891de5e65f85f3a45edc5d37be3f19c11aca
Chad Jemmett <chad.jemmett@gmail.com>
2019-02-21 13:57:51 -0700

    Added a matrix to visualize the stair climbing problem

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,7 +2,30 @@
 
 import sys
 
+
 def climbing_stairs(n, cache=None):
+    # So for every stair the kid touches. She can do it three different ways.
+
+    # The kid can touch every stair once. 
+    # or The kid can touch every other stair.
+    # Or every third stair.
+    # But if the kid hops three stairs,
+
+    Every stair except the top 3 and bottom 3 has can be reached by 1 2 or 3 hops.
+    Two can only be reached by one hop.
+    Top stair.
+    5 stairs.
+    Stair 1.
+        jump to the
+
+
+
+
+
+
+
+
+
   pass 
 
 
