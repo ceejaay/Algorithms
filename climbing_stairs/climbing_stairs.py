ee2a0760e8cbae3c546bcf0b14d7c0b77f457724
Chad Jemmett <chad.jemmett@gmail.com>
2019-02-21 15:57:54 -0700

    Trying to understand fibonacci

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -4,6 +4,7 @@ import sys
 
 
 def climbing_stairs(n, cache=None):
+    pass
     # So for every stair the kid touches. She can do it three different ways.
 
     # The kid can touch every stair once. 
@@ -11,12 +12,36 @@ def climbing_stairs(n, cache=None):
     # Or every third stair.
     # But if the kid hops three stairs,
 
-    Every stair except the top 3 and bottom 3 has can be reached by 1 2 or 3 hops.
-    Two can only be reached by one hop.
-    Top stair.
-    5 stairs.
-    Stair 1.
-        jump to the
+    # Every stair except the top 3 and bottom 3 has can be reached by 1 2 or 3 hops.
+    # Two can only be reached by one hop.
+    # Top stair.
+    # 5 stairs.
+    # Stair 1.
+    #     jump to the
+
+    # for every stair we need to calculate down to 0 from 3.o
+    #     for every step. We increment by both 1 2, & 3 unless you are the bottom steps.
+
+# for each number we have to calculate the steps.
+    # keeping in mind the end and beginning of the stairs.
+
+    # number + 3
+    # number + 2
+    # number + 1
+    # Starting a the top.
+    # The possible number combinations requried to reach the stair is this.
+    # 9 
+    # the steps are 
+
+# def fib(n):
+#     count = 0
+#     if n <= 0:
+#         return "enter a positive number"
+#     elif n == 1:
+#         return 0
+#     else:
+#         while count < n
+#             return
 
 
 
@@ -26,7 +51,11 @@ def climbing_stairs(n, cache=None):
 
 
 
-  pass 
+
+
+
+
+  # pass
 
 
 if __name__ == "__main__":
